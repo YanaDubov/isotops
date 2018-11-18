cbb947f0bc6ffbc8e364f03c2f312fa4f2fed716
Yulia <kisliuk-y@mail.ru>
2018-11-18 15:02:44 +0300

    +

--- fxRad/src/sample/Controller.java
+++ fxRad/src/sample/Controller.java
@@ -41,38 +41,62 @@ public class Controller {
 
     public void initialize(){
 
-
-
-        Isotop polonium210 = new Isotop("Polonium 210", (float) 138.0, "day",0.0,(float)208.98);
+        Isotop polonium210 = new Isotop("Polonium 210", (float) 138.0, "day",0.0,(float)210);
         list.add(polonium210);
         Isotop iodine131=new Isotop("Iodine 131", (float) 8.0, "day",0.0,(float)131);
         list.add(iodine131);
-        Isotop gadolinium153=new Isotop("Gadolinium 153", (float) 240.0,"day",0.0,(float)157.25);
+        Isotop gadolinium153=new Isotop("Gadolinium 153", (float) 240.0,"day",0.0,(float)153);
         list.add(gadolinium153);
         Isotop tritinium=new Isotop("Tritinum H3", (float) 12.3,"year",0.0,(float)3.016);
         list.add(tritinium);
-        Isotop fluorine18 = new Isotop("Fluorine 18", (float) 0.92, "hour",0.0,(float)18.0);
+        Isotop fluorine18 = new Isotop("Fluorine 18", (float) 0.92, "hour",0.0,(float)18);
         list.add(fluorine18);
-        Isotop cobalt60 = new Isotop("Cobalt 60", (float) 5.3, "year",0.0,(float)59.933);
+        Isotop cobalt60 = new Isotop("Cobalt 60", (float) 5.3, "year",0.0,(float)60);
         list.add(cobalt60);
-        Isotop strontium90 = new Isotop("Strontium 90", (float) 28.8, "year",0.0,(float)89.907);
+        Isotop strontium90 = new Isotop("Strontium 90", (float) 28.8, "year",0.0,(float)90);
         list.add(strontium90);
-        Isotop technetium99 = new Isotop("Technetium 99", (float) 6.0, "hour",0.0,(float)98.906);
+        Isotop technetium99 = new Isotop("Technetium 99", (float) 6.0, "hour",0.0,(float)99);
         list.add(technetium99);
-        Isotop xenon135 = new Isotop("Xenon 135", (float) 9.1, "hour",0.0,(float)131.293);
+        Isotop xenon135 = new Isotop("Xenon 135", (float) 9.1, "hour",0.0,(float)135);
         list.add(xenon135);
-        Isotop caesium137 = new Isotop("Caesium 137", (float) 30.2, "year",0.0,(float)136.9);
+        Isotop caesium137 = new Isotop("Caesium 137", (float) 30.2, "year",0.0,(float)137);
         list.add(caesium137);
-        Isotop radon222 = new Isotop("Radon 222", (float) 3.8, "day",0.0,(float)222.017);
+        Isotop radon222 = new Isotop("Radon 222", (float) 3.8, "day",0.0,(float)222);
         list.add(radon222);
-        Isotop plutonium238 = new Isotop("Plutonium 238", (float) 87.7, "year",0.0,(float)238.049);
+        Isotop plutonium238 = new Isotop("Plutonium 238", (float) 87.7, "year",0.0,(float)238);
         list.add(plutonium238);
-        Isotop americium241 = new Isotop("Americium 241", (float) 432, "year",0.0,(float)241.056);
+        Isotop americium241 = new Isotop("Americium 241", (float) 432, "year",0.0,(float)241);
         list.add(americium241);
-        Isotop californium252 = new Isotop("Californium 252", (float) 2.64, "year",0.0,(float)251.0796);
+        Isotop californium252 = new Isotop("Californium 252", (float) 2.64, "year",0.0,(float)252);
         list.add(californium252);
-        Isotop caesium134 = new Isotop("Caesium 134", (float) 2.0, "year",0.0,(float)133.9067);
+        Isotop caesium134 = new Isotop("Caesium 134", (float) 2.0, "year",0.0,(float)134);
         list.add(caesium134);
+        Isotop krypton85 = new Isotop("Krypton 85", (float) 10.72, "year",0.0,(float)85);
+        list.add(krypton85);
+        Isotop tellurium132 = new Isotop("Tellurium 132", (float) 3.26, "day",0.0,(float)132);
+        list.add(tellurium132);
+        Isotop strontium89 = new Isotop("Strontium 89", (float) 50.5, "day",0.0,(float)89);
+        list.add(strontium89);
+        Isotop rubidium103 = new Isotop("Rubidium 103", (float) 39.3, "day",0.0,(float)103);
+        list.add(rubidium103);
+        Isotop rubidium106 = new Isotop("Rubidium 106", (float) 368, "day",0.0,(float)106);
+        list.add(rubidium106);
+        Isotop barium140 = new Isotop("Barium 140", (float) 12.7, "day",0.0,(float)140);
+        list.add(barium140);
+        Isotop zirconium95 = new Isotop("Zirconium 95", (float) 94, "day",0.0,(float)95);
+        list.add(zirconium95);
+        Isotop molybdenum99 = new Isotop("Molybdenum 99", (float) 2.75, "day",0.0,(float)99);
+        list.add(molybdenum99);
+        Isotop cerium141 = new Isotop("Cerium 141", (float) 32.5, "day",0.0,(float)141);
+        list.add(cerium141);
+        Isotop cerium144 = new Isotop("Cerium 144", (float) 284, "day",0.0,(float)144);
+        list.add(cerium144);
+        Isotop neptunium239 = new Isotop("Neptunium 239", (float) 2.36, "day",0.0,(float)239);
+        list.add(neptunium239);
+        Isotop plutonium241 = new Isotop("Plutonium 241", (float) 14.4, "year",0.0,(float)241);
+        list.add(plutonium241);
+
+
 
         ArrayList <String> nameIsot =new ArrayList<>();
 
